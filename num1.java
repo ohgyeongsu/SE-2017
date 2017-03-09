486699bba2cf440a2012aa7353823f5ed451d3c3
ohgyeongsu <dhrudtn@gmail.com>
2017-03-09 14:00:15 +0900

    java

---
+++ num1.java
@@ -0,0 +1,16 @@
+package org.test.study;
+import java.util.Scanner;
+public class num1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1을 입력하면 내 이름을 출력해라
+		Scanner number = new Scanner(System.in);
+		
+		if(number.nextInt() == 1)
+		{
+			System.out.println("ohgyeongsu");
+		}
+	}
+
+}
